2ee54517a05e8eb2156eda62020fd38a7ff87830
Foram Rafaliya <foramrafaliya3012@gmail.com>
2026-05-07 10:02:58 +0000

    Day 2: Added threshold alerts for CPU, RAM, Disk

--- monitor.py
+++ monitor.py
@@ -11,4 +11,13 @@ def get_stats():
     print(f"RAM Usage    : {ram.percent}%  ({ram.used // 1024**2} MB used)")
     print(f"Disk Usage   : {disk.percent}% ({disk.used // 1024**3} GB used)")
     
+    if (cpu >= 80):
+        print("alert : cpu high!!!")
+
+    if (ram.percent > 80):
+        print("alert : ram high!!!")
+
+    if (disk.percent > 90):
+        print("alert : disk full!!!")    
+   
 get_stats()
