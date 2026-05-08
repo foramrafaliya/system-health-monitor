2824321bcabcfbbf3fbfb7ae8efd04dcd591e15e
Foram Rafaliya <foramrafaliya3012@gmail.com>
2026-05-08 09:05:20 +0000

    Day 5: Added email alerts via SMTP

--- monitor.py
+++ monitor.py
@@ -2,12 +2,33 @@ import psutil
 import time
 import datetime
 import logging
+import smtplib
+from email.mime.text import MIMEText
 
 logging.basicConfig(
     filename='health.log',
     level=logging.INFO,
     format='%(asctime)s - %(message)s'
 )
+
+def send_alert(subject, message):
+    sender = "foramrafaliya3012@gmail.com"
+    receiver = "foramrafaliya3012@gmail.com"
+    password = "jhbv ubvt mvtj rtvb"
+    
+    msg = MIMEText(message)
+    msg['Subject'] = subject
+    msg['From'] = sender
+    msg['To'] = receiver
+
+    with smtplib.SMTP('smtp.gmail.com', 587) as server:
+        server.starttls()
+        server.login("foramrafaliya3012@gmail.com", "jhbv ubvt mvtj rtvb")
+        server.sendmail("foramrafaliya3012@gmail.com", "foramrafaliya3012@gmail.com", msg.as_string())
+    
+    print("Alert email sent!")
+    
+  
 def get_stats():
     cpu = psutil.cpu_percent()
     ram = psutil.virtual_memory()
@@ -25,14 +46,17 @@ def get_stats():
     if (cpu >= 80):
         logging.info("alert : cpu high!!!")
         print("alert : cpu high!!!")
+        send_alert("CPU Alert!", f"CPU usage is {cpu}% - exceeded 80% threshold!")
 
     if (ram.percent > 80):
         logging.info("alert : ram high!!!")
         print("alert : ram high!!!")
+        send_alert("RAM Alert!", f"RAM usage is {ram.percent}% - exceeded 80% threshold!")
 
     if (disk.percent > 90):
         logging.info("alert : disk full!!!")  
-        print("alert : disk full!!!")    
+        print("alert : disk full!!!")  
+        send_alert("DISK Alert!", f"DISK usage is {disk.percent}% - exceeded 90% threshold!")  
 
 while True:
      get_stats()
